00adcc4b4c45886ffac4b32e755868d0195f81bb
lgeniaux <lgeniaux17@gmail.com>
2022-09-27 15:45:46 +0200

    2nd signal handling test

--- sig.c
+++ sig.c
@@ -6,15 +6,28 @@
 /*   By: lgeniaux <lgeniaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 14:26:09 by lgeniaux          #+#    #+#             */
-/*   Updated: 2022/09/27 14:26:09 by lgeniaux         ###   ########.fr       */
+/*   Updated: 2022/09/27 15:45:29 by lgeniaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//function to handle ctrl-c signal
+// error g_variable, not used yet
+int g_code;
 
-void    ft_handle_ctrl_c_signal(int sig)
+// ctrl+c handler
+void	sigint_handler(int sig)
 {
-    
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	(void) sig;
+}
+
+// catching ctrl+c and redirect ctrl+\ to IGN signal
+void	signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
