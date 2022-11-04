b76ba680f6bc3204bdb9303f077c6725dbe76b85
lgeniaux <lgeniaux17@gmail.com>
2022-11-04 16:01:05 +0100

    Remove ^C when ctrl-c (bug on first occurence)

--- srcs/signals/sig.c
+++ srcs/signals/sig.c
@@ -6,18 +6,24 @@
 /*   By: lgeniaux <lgeniaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 14:26:09 by lgeniaux          #+#    #+#             */
-/*   Updated: 2022/11/04 13:37:43 by lgeniaux         ###   ########.fr       */
+/*   Updated: 2022/11/04 15:09:06 by lgeniaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "termios.h"
 
-// ctrl+c handler (Need to delete ^C printing)
+// ctrl+c handler
 void	sigint_handler(int sig)
 {
+	struct termios t;
+	
+	tcgetattr(0, &t);
+    t.c_lflag &= ~ECHOCTL;
+    tcsetattr(0, TCSANOW, &t);
 	printf("\n");
 	rl_on_new_line();
-	rl_replace_line("", 0);   //BS library bug 
+	rl_replace_line("", 0);
 	rl_redisplay();
 	(void) sig;
 }
