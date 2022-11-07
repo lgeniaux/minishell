cbd4a3887f38e9fcc4758b858d98a52cb004fff1
Alexis Lavaud <alavaud@student.42lyon.fr>
2022-11-07 17:00:47 +0100

    Fixed variable substitution problem (resolved to nothing if the variable is not found or empty)

--- srcs/parsing/resolve.c
+++ srcs/parsing/resolve.c
@@ -6,7 +6,7 @@
 /*   By: alavaud <alavaud@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:59:46 by alavaud           #+#    #+#             */
-/*   Updated: 2022/11/02 14:21:37 by alavaud          ###   ########lyon.fr   */
+/*   Updated: 2022/11/07 16:57:19 by alavaud          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,11 @@ char	*append_var(char **resolved, char *cmdline, char **env)
 			var = ft_getenv(env, cmdline + 1, len - 1);
 	}
 	if (var)
+	{
 		*resolved = str_append(*resolved, var, ft_strlen(var));
+		if (NULL == *resolved)
+			return (NULL);
+	}
 	return (cmdline + len);
 }
 
@@ -90,8 +94,10 @@ char	*resolve_vars(char *cmdline, char **env)
 			else
 				++i;
 			resolved = str_append(resolved, cmdline++, i);
+			if (!resolved)
+				break ;
 		}
-		if (!resolved)
+		if (!cmdline)
 			break ;
 	}
 	return (resolved);
